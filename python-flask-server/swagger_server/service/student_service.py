bb3c00895135f9abb5ab0975e79afece21010d94
Rasjaad <rasjaad10@msn.com>
2021-02-11 14:10:53 +0100

    Fixed unit tests

--- python-flask-server/swagger_server/service/student_service.py
+++ python-flask-server/swagger_server/service/student_service.py
@@ -24,7 +24,8 @@ def add_student(student):
     res = student_db.search(query)
     if res:
         return 'already exists', 409
-
+    if not student.last_name or not student.first_name:
+        return 'Invalid input', 405
     doc_id = student_db.insert(student.to_dict())
     student.student_id = doc_id
     return student.student_id
@@ -33,15 +34,19 @@ def add_student(student):
 def get_student_by_id(student_id, subject):
     student = student_db.get(doc_id=int(student_id))
     if not student:
-        return student
+        return 'not found', 404
     student = Student.from_dict(student)
     if not subject:
         return student
+    elif subject in student.grades:
+        return student
+    else:
+        return 'not found', 404
 
 
 def delete_student(student_id):
     student = student_db.get(doc_id=int(student_id))
     if not student:
-        return student
+        return 'not found', 404
     student_db.remove(doc_ids=[int(student_id)])
     return student_id
